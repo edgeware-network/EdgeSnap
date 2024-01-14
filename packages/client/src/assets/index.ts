4e1bf40deef513c8b212dee7cc9e6cb4a55b617e
pranavcpawar <pranavcpawar@outlook.com>
2024-01-15 02:48:15 +0530

    add assets

---
+++ packages/client/src/assets/index.ts
@@ -0,0 +1,12 @@
+import polkadot  from "../assets/polkadot.svg";
+import edgeware  from "../assets/edgeware.svg";
+import kusama  from "../assets/kusama.svg";
+import astar  from "../assets/astar.svg";
+
+
+export {
+  polkadot,
+  edgeware,
+  kusama,
+  astar
+};
