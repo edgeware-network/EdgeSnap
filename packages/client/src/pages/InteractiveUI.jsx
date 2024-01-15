2968baabd00f3f228d8f0ab8d5a3d66e1667c05a
pranavcpawar <pranavcpawar@outlook.com>
2024-01-15 19:47:10 +0530

    initial home and ui page

--- packages/client/src/pages/InteractiveUI.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InteractiveUI = () => {
-  return (
-    <div>InteractiveUI</div>
-  )
-}
-
-export default InteractiveUI
---
+++ packages/client/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from "./Home";
+export { default as SnapStore } from "./SnapStore";
+export { default as InteractiveUI } from "./InteractiveUI";
