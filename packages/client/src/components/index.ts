a2f76225d54de1001144dff4796bbd766ccbfe2d
pranavcpawar <pranavcpawar@outlook.com>
2024-01-15 19:45:57 +0530

    add imports

---
+++ packages/client/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Account } from "./Account";
+export { default as LatestBlock } from "./LatestBlock";
+export { default as Navbar } from "./Navbar";
+export { default as SignMessage } from "./SignMessage";
+export { default as SnapCard } from "./SnapCard";
+export { default as Snaps } from "./Snaps";
+export { default as Transfer } from "./Transfer";
+export { default as TxTable } from "./TxTable";
