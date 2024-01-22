9282b1d3fb140aeb7d8ef91710e41dd42e5f3b7f
pranavcpawar <pranavcpawar@outlook.com>
2024-01-22 16:19:51 +0530

    add tx log component

--- packages/client/src/components/TxTable.tsx
+++ packages/client/src/components/TxTable.tsx
@@ -1,8 +1,17 @@
+import { formatBalance } from '@polkadot/util';
+import type { Transaction } from '@chainsafe/metamask-polkadot-types';
+import { shortAddress } from '../../services/format';
 
-const TxTable = () => {
-  return (
-    <div>TxTable</div>
-  )
+export interface TxTableProps {
+  txs: Transaction[];
 }
+const TxTable = (props: TxTableProps): React.JSX.Element => {
+  return (
+    <div className='flex w-[70vw] flex-col font-unbounded items-start justify-center gap-4 shadow-[4px_4px] border-2 border-[#cbcbcb] shadow-secondary-800 p-4'>
+      <h1 className='text-primary-600 text-xl'>Transaction Logs</h1>
+      
+    </div>
+  );
+};
 
-export default TxTable
+export default TxTable;
