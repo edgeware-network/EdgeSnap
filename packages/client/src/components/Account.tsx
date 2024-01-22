067353d15508f8d4949d645092a094293b6d87e7
pranavcpawar <pranavcpawar@outlook.com>
2024-01-22 16:19:30 +0530

    add account component

--- packages/client/src/components/Account.tsx
+++ packages/client/src/components/Account.tsx
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { formatBalance } from '@polkadot/util';
-import { MetaMaskContext } from '../../context/metamask';
-import { getCurrency } from '../../services/format';
+import { getCurrency, shortAddress } from '../../services/format';
 
 export interface AccountProps {
   address: string;
@@ -11,22 +10,43 @@ export interface AccountProps {
 }
 
 const Account = (props: AccountProps): React.JSX.Element => {
-  const [state] = useContext(MetaMaskContext);
+  const [address, setAddress] = React.useState<string>('');
+  const [pubkey, setPublicKey] = React.useState<string>('');
+  const [balance, setBalance] = React.useState<string>('');
 
-  const handleExport = async (): Promise<void> => {
-    if (!state.polkadotSnap.snap) return;
-    const api = state.polkadotSnap.snap.getMetamaskSnapApi();
-    const privateKey = await api.exportSeed();
-    alert(privateKey);
-  };
+  const handleAddress = () => {
+    setAddress(shortAddress(props.address));
+  }
+
+  const handlePubicKey = () => {
+    setPublicKey(shortAddress(props.pubkey));
+  }
+  const handleBalance = () => {
+    const balance = formatBalance(props.balance, { decimals: 12, withSi: true, withUnit: getCurrency(props.network)});
+    setBalance(balance);
+  }
 
   return (
-    <div>
-      <h1>{props.address}</h1>
-      <h1>{formatBalance(props.balance, {decimals: 18, withSi: true, withUnit: getCurrency(props.network)})}</h1>
-      <h1>{props.pubkey}</h1>
-      <h1>{props.network}</h1>
-      <button onClick={handleExport}>Export Private Key</button>
+    <div className='flex w-full flex-col font-unbounded items-start justify-center gap-5 shadow-[4px_4px] border-2 border-[#cbcbcb] shadow-secondary-800 p-4'>
+      <h1 className='text-primary-600 text-xl'>Account Details</h1>
+      <div className='flex sm:flex-row flex-col gap-2 p-1 sm:items-center items-start justify-center'>
+        <button onClick={handleAddress} className='text-xs bg-primary-650 px-4 py-2 rounded-[8px] active:scale-95'>Get Account</button>
+        <h3 className='text-secondary-500 text-sm flex relative left-2'>
+          {address}
+        </h3>
+      </div>
+      <div className='flex sm:flex-row flex-col gap-2 p-1 sm:items-center items-start justify-center'>
+        <button onClick={handleBalance} className='text-xs bg-primary-650 px-4 py-2 rounded-[8px] active:scale-95'>Get Balance</button>
+        <h3 className="text-sm text-secondary-500 flex relative left-2">
+          {balance}
+        </h3>
+      </div>
+      <div className='flex sm:flex-row flex-col gap-2 p-1 sm:items-center items-start justify-center'>
+        <button onClick={handlePubicKey} className='text-xs bg-primary-650 px-4 py-2 rounded-[8px] active:scale-95'>Get PubKey</button>
+        <h3 className='text-secondary-500 text-sm flex relative left-2'>
+          {pubkey}
+        </h3>
+      </div>
     </div>
   );
 };
