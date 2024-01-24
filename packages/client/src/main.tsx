f8fe1b473a0a74007d5d5482687630e9f9fe6209
pranavcpawar <pranavcpawar@outlook.com>
2024-01-24 12:07:52 +0530

    wrap app with metamask provider

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { MetaMaskContextProvider } from '../context/metamask.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <MetaMaskContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </MetaMaskContextProvider>
   </React.StrictMode>,
 );
