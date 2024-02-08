3a3fb5d3299afabed356268ff0f6df95dd2e41fb
pranavcpawar <pranavcpawar@outlook.com>
2024-02-08 19:14:57 +0530

    remove redundant code

--- packages/client/src/components/ConnectWallet.tsx
+++ packages/client/src/components/ConnectWallet.tsx
@@ -1,100 +1,9 @@
-// import React, { useCallback, useContext, useEffect } from "react"
-// import { MetaMaskContext, MetamaskActions } from "../../context/metamask";
-// import { shortAddress } from "../../services/format";
-// import { initiatePolkadotSnap } from "../../services/metamask";
-
-// const ConnectWallet = (): React.JSX.Element => {
-//     const [state, dispatch ] = useContext(MetaMaskContext);
-//     const [, setAddress] = React.useState<string>('')
-    
-//     const updateWallet = useCallback(async () => {
-//         if (window.ethereum && state.connectWallet.isManuallyDisConnected) {
-//             try {
-//                 const accounts: string[] = await window.ethereum.request({
-//                     method: "eth_accounts",
-//                 });
-//                 if (accounts.length > 0) {
-//                     setAddress(accounts[0]);
-//                     dispatch({ type: MetamaskActions.SET_CONNECTED_STATUS, payload: { isConnected: true, address: accounts[0] } });
-//                     const installResult = await initiatePolkadotSnap();
-//                     if (!installResult.isSnapInstalled) {
-//                         dispatch({
-//                             payload: {
-//                                 isInstalled: false,
-//                                 message: 'Please accept snap installation prompt'
-//                             },
-//                             type: MetamaskActions.SET_INSTALLED_STATUS
-//                         });
-//                     } else {
-//                         dispatch({
-//                             payload: { isInstalled: true, snap: installResult.snap },
-//                             type: MetamaskActions.SET_INSTALLED_STATUS
-//                         });
-//                     }
-//                 }
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         }
-//     }, [dispatch, state.connectWallet.isManuallyDisConnected]);
-
-//     useEffect(() => {
-//         updateWallet();
-//     }, [updateWallet]);
-
-//     const connectWallet = async () => {
-//         try {
-//             if(window.ethereum && !state.connectWallet.isManuallyDisConnected ) {
-//                 dispatch({type: MetamaskActions.SET_DISCONNECTED_STATUS , payload: {isManuallyDisconnected: false}});
-//                 await window.ethereum.request({
-//                     method: "wallet_requestPermissions",
-//                     params: [{ eth_accounts: {} }],
-//                 });
-//                 const accounts: string[] = await window.ethereum.request({
-//                     method: "eth_requestAccounts",
-//                 })
-//                 setAddress(accounts[0]);
-//                 dispatch({type: MetamaskActions.SET_CONNECTED_STATUS, payload: {isConnected: true, address: accounts[0]}});
-//                 const installResult = await initiatePolkadotSnap();
-//                 if (!installResult.isSnapInstalled) {
-//                   dispatch({
-//                     payload: {
-//                       isInstalled: false,
-//                       message: 'Please accept snap installation prompt'
-//                     },
-//                     type: MetamaskActions.SET_INSTALLED_STATUS
-//                   });
-//                 } else {
-//                   dispatch({
-//                     payload: { isInstalled: true, snap: installResult.snap },
-//                     type: MetamaskActions.SET_INSTALLED_STATUS
-//                   });
-//                 }
-//             }
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-    
-//   return (
-//     <button onClick={connectWallet} className='px-4 py-1 bg-inherit font-unbounded font-medium hover:bg-[#131313] active:scale-95 border border-[#353535] rounded-[6px]'>
-//         {state.connectWallet.isConnected ? (
-//             <div className="flex items-center justify-center gap-1">
-//                 <h3 className="text-sm font-bold font-unbounded">{shortAddress(state.connectWallet.address)}</h3>
-//             </div>
-//         ) : (
-// 					"Connect Wallet"
-// 					)}
-//     </button>
-//   );
-// };
-
-// export default ConnectWallet
-
 import React, { useCallback, useContext, useEffect } from "react"
 import { MetaMaskContext, MetamaskActions } from "../../context/metamask";
 import { shortAddress } from "../../services/format";
 import { initiatePolkadotSnap } from "../../services/metamask";
+import "react-toastify/ReactToastify.css";
+import { toast } from "react-toastify";
 
 const ConnectWallet = (): React.JSX.Element => {
     const [state, dispatch ] = useContext(MetaMaskContext);
@@ -150,21 +59,12 @@ const ConnectWallet = (): React.JSX.Element => {
                 })
                 setAddress(accounts[0]);
                 dispatch({type: MetamaskActions.SET_CONNECTED_STATUS, payload: {isConnected: true, address: accounts[0]}});
-                const installResult = await initiatePolkadotSnap();
-                if (!installResult.isSnapInstalled) {
-                  dispatch({
-                    payload: {
-                      isInstalled: false,
-                      message: 'Please accept snap installation prompt'
-                    },
-                    type: MetamaskActions.SET_INSTALLED_STATUS
-                  });
-                } else {
-                  dispatch({
-                    payload: { isInstalled: true, snap: installResult.snap },
-                    type: MetamaskActions.SET_INSTALLED_STATUS
-                  });
-                }
+								toast.success('Successfully connected your wallet!');
+								if (state.polkadotSnap.isInstalled) {
+									toast.info("Snap is already installed!");
+								} else {
+									toast.info("Install the snap!")
+								}
             }
         } catch (error) {
             console.log(error);
@@ -172,16 +72,16 @@ const ConnectWallet = (): React.JSX.Element => {
     };
 
   return (
-    <button onClick={connectWallet} className='px-4 py-1 bg-inherit font-unbounded font-medium hover:bg-[#131313] active:scale-95 border border-[#353535] rounded-[6px]'>
-        {state.connectWallet.isConnected ? (
-            <div className="flex items-center justify-center gap-1">
-                <h3 className="text-sm font-bold font-unbounded">{shortAddress(state.connectWallet.address)}</h3>
-            </div>
-        ) : (
+		<button onClick={connectWallet} className='px-4 py-1 bg-inherit font-unbounded font-medium hover:bg-[#131313] active:scale-95 border border-[#353535] rounded-[6px]'>
+				{state.connectWallet.isConnected ? (
+						<div className="flex items-center justify-center gap-1">
+								<h3 className="text-sm font-bold font-unbounded">{shortAddress(state.connectWallet.address)}</h3>
+						</div>
+				) : (
 					"Connect Wallet"
 					)}
-    </button>
-  );
+		</button>
+ );
 };
 
-export default ConnectWallet
+export default ConnectWallet;
